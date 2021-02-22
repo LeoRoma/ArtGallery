e5d4e8bb8dfc9ff1f64850f1f7c66344f1a2eef5
LeoRoma <xiajtn@gmail.com>
2021-02-22 13:41:45 +0000

    Right side navbar toggle button animation added

--- src/script.js
+++ src/script.js
@@ -5,8 +5,9 @@ let landingPageContainerHeight = landingPageContainer.scrollHeight;
 const rightSidenavHeader = document.querySelector('.right-sidenav-header');
 let rightSidenavHeaderWidth = rightSidenavHeader.offsetWidth;
 console.log(rightSidenavHeaderWidth);
-const drawer = document.querySelector('.drawer');
-let drawerWidth = drawer.offsetWidth;
+const drawerButton = document.querySelector('.drawer-button');
+const rightSideNavInner = document.querySelector('.right-sidenav-inner')
+// let drawerWidth = drawer.offsetWidth;
 
 // body.addEventListener('onresize', () => {
 //     landingPageContainerHeight = landingPageContainer.scrollHeight;
@@ -15,8 +16,13 @@ let drawerWidth = drawer.offsetWidth;
 
 document.addEventListener('scroll', () => {
     rightSidenavHeader.style.right = scrollTop.scrollTop >= 1 ? `-${rightSidenavHeaderWidth}px` : '0px';
-    drawer.style.right = scrollTop.scrollTop >= 1 ? '0px' : `-100px`;
+    drawerButton.style.right = scrollTop.scrollTop >= 1 ? '-15px' : `-150px`;
     landingPageContainer.style.display = scrollTop.scrollTop >= landingPageContainerHeight ? 'none' : 'block';
     console.log(typeof scrollTop.scrollTop)
 })
 
+
+drawerButton.addEventListener('click', () => {
+    rightSideNavInner.classList.toggle('open');
+    drawerButton.classList.toggle('change');
+})
