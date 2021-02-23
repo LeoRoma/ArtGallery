935290b5573d1d150f08d12a349c03fa168d2180
LeoRoma <xiajtn@gmail.com>
2021-02-23 18:27:13 +0000

    Adding circle effect when opening side navbar

--- src/script.js
+++ src/script.js
@@ -6,28 +6,21 @@ const rightSidenavHeader = document.querySelector('.right-sidenav-header');
 let rightSidenavHeaderWidth = rightSidenavHeader.offsetWidth;
 console.log(rightSidenavHeaderWidth);
 const drawerButton = document.querySelector('.drawer-button');
-const rightSideNavInner = document.querySelector('.right-sidenav-inner')
-// let drawerWidth = drawer.offsetWidth;
-
-// body.addEventListener('onresize', () => {
-//     landingPageContainerHeight = landingPageContainer.scrollHeight;
-//     rightSidenavHeaderWidth = rightSidenavHeader.offsetWidth;
-// })
+const rightSideNavInner = document.querySelector('.right-sidenav-inner');
+const rightSidenavContainer = document.querySelector('.right-sidenav-container');
 
 window.addEventListener('scroll', () => {
     rightSidenavHeader.style.right = scrollTop.scrollTop >= 1 ? `-${rightSidenavHeaderWidth}px` : '0px';
     drawerButton.style.right = scrollTop.scrollTop >= 1 ? '-15px' : `-150px`;
-    // landingPageContainer.style.display = scrollTop.scrollTop >= landingPageContainerHeight ? 'none' : 'block';
-    // let currentScroll = window.pageYOffset || scrollTop.scrollTop ||
-    //     body.scrollTop || 0;
-
     landingPageContainer.style.opacity = -scrollTop.scrollTop / (landingPageContainerHeight) + 1
 })
 
 
 drawerButton.addEventListener('click', () => {
     rightSideNavInner.classList.toggle('open');
+    
     drawerButton.classList.toggle('change');
-})
+    rightSidenavContainer.classList.toggle('circle'); 
 
+})
 
